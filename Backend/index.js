d61c5482acb745bffc7a28f72770d50df4810da1
Virang Jain <virangjain355@gmail.com>
2025-04-06 01:36:05 +0530

    create dev command

--- Backend/index.js
+++ Backend/index.js
@@ -20,10 +20,22 @@ const PORT = process.env.PORT;
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(cookieParser());
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://creativethread-c3a89.web.app"
+];
+
 app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  }))
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+}));
+
 
 // Creating All API's
 app.use("/api/auth", authRoutes);
