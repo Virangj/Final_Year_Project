0a2ae628617ce843e4c49650d4efaa180d4c0a56
Virang Jain <virangjain355@gmail.com>
2025-08-03 01:27:45 +0530

    add new origin 1

--- Backend/index.js
+++ Backend/index.js
@@ -25,6 +25,7 @@ app.use(cookieParser());
 const allowedOrigins = [
   "http://localhost:5173",
   "https://creative-theards.vercel.app",
+  "https://creativethreads.vercel.app",
   "https://creativethreads.netlify.app",
 ];
 app.use(
